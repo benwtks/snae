81c51989e195b6d21db82bb8be8cae916f30dcf1
Ben Watkins <contact@benwtks.com>
2020-04-11 23:47:30 +0100

    Restructure assets, adding sass

--- functions.php
+++ functions.php
@@ -143,9 +143,9 @@ add_action( 'widgets_init', 'snae_widgets_init' );
 function snae_scripts() {
 	wp_enqueue_style( 'snae-style', get_stylesheet_uri(), array(), _S_VERSION );
 
-	wp_enqueue_script( 'snae-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_script( 'snae-navigation', get_template_directory_uri() . '/assets/js/navigation.js', array(), _S_VERSION, true );
 
-	wp_enqueue_script( 'snae-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), _S_VERSION, true );
+	wp_enqueue_script( 'snae-skip-link-focus-fix', get_template_directory_uri() . '/assets/js/skip-link-focus-fix.js', array(), _S_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
