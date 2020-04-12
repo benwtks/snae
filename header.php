9e1178bdb352648f4d1a8e21dd7f3ad26609ea16
Ben Watkins <contact@benwtks.com>
2020-04-12 18:26:38 +0100

    Begin working on header

--- header.php
+++ header.php
@@ -27,22 +27,14 @@
 
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
+			<div class="social">
+			</div>
+			<div class="logo">
+				<?php the_custom_logo(); ?>
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$snae_description = get_bloginfo( 'description', 'display' );
-			if ( $snae_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $snae_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
+			</div>
+			<div class="nav-icons">
+			</div>
 		</div>
 
 		<nav id="site-navigation" class="main-navigation">
