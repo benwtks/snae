e05bf205f7d822f55119bb7d7bd843c77c130aeb
Ben Watkins <contact@benwtks.com>
2020-04-12 18:44:14 +0100

    Remove old footer

--- footer.php
+++ footer.php
@@ -13,20 +13,7 @@
 
 	</div>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'snae' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'snae' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'snae' ), 'snae', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div>
+	<footer class="site-footer">
 	</footer>
 </div>
 
