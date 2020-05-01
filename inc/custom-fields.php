2a38109ca99238cdac73e2174ba20a7492b08849
Ben Watkins <contact@benwtks.com>
2020-05-01 02:47:04 +0100

    Add carbonfields

---
+++ inc/custom-fields.php
@@ -0,0 +1,34 @@
+<?php
+use Carbon_Fields\Container;
+use Carbon_Fields\Field;
+
+function hide_editor() {
+    $post_id = $_GET['post'] ? $_GET['post'] : $_POST['post_ID'] ;
+    if( !isset( $post_id ) ) return;
+
+    $title = get_the_title($post_id);
+     
+    if($title == 'Contact'){ // edit the template name
+        remove_post_type_support('page', 'editor');
+        remove_post_type_support('page', 'revisions');
+        remove_post_type_support('page', 'custom-fields');
+        remove_post_type_support('page', 'comments');
+        remove_post_type_support('page', 'author');
+        remove_post_type_support('page', 'page-attributes');
+        remove_post_type_support('page', 'thumbnail');
+    }
+}
+
+add_action( 'carbon_fields_register_fields', 'crb_attach_theme_options' );
+function crb_attach_theme_options() {
+    Container::make( 'theme_options', __( 'Theme Options', 'crb' ) )
+        ->add_fields( array(
+            Field::make( 'textarea', 'crb_theme_address', 'Address' ),
+        ) );
+}
+
+add_action( 'after_setup_theme', 'crb_load' );
+function crb_load() {
+    require_once( ABSPATH . '/vendor/autoload.php' );
+    \Carbon_Fields\Carbon_Fields::boot();
+}
