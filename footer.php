8fd8313f99bbc0773afe205160f784595d779fc4
Ben Watkins <contact@benwtks.com>
2020-05-01 02:45:39 +0100

    Add basic structure for footer

--- footer.php
+++ footer.php
@@ -14,6 +14,27 @@
 	</div>
 
 	<footer class="site-footer">
+		<div class="company-details">
+			<div class="logo">
+					<?php if (has_custom_logo()): ?>
+						<?php the_custom_logo() ?>
+					<?php else: ?>
+						<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+					<?php endif; ?>
+			</div>
+			<div class="address">
+				<span></span>
+			</div>
+			<span class="telephone-no"></span>
+			<span class="email"></span>
+			<span class="copyright"></span>
+		</div>
+		<div class="explore">
+			<div class="newsletter">
+			</div>
+			<div class="links">
+			</div>
+		</div>
 	</footer>
 </div>
 
