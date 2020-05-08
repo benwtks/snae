afd290ea0d1a254d332cebf4fd0dbc26ddc08e39
Ben Watkins <contact@benwtks.com>
2020-05-08 02:58:37 +0100

    refine footer

--- footer.php
+++ footer.php
@@ -14,57 +14,59 @@
 	</div>
 
 	<footer class="site-footer">
-		<div class="company-details">
-			<div class="logo">
-					<?php if (has_custom_logo()): ?>
-						<?php the_custom_logo() ?>
-					<?php else: ?>
-						<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<?php endif; ?>
+		<div class="content-wrapper">
+			<div class="company-details">
+				<div class="logo">
+						<?php if (has_custom_logo()): ?>
+							<?php the_custom_logo() ?>
+						<?php else: ?>
+							<h2 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h2>
+						<?php endif; ?>
+				</div>
+				<div class="address">
+					<span class="detail-value"><?php echo nl2br (carbon_get_theme_option('crb_theme_address')) ?></span>
+				</div>
+				<div class="telephone-no">
+					<span class="detail-title">Telephone:</span>
+					<span class="detail-value"><?php echo carbon_get_theme_option('crb_theme_telephone')?></span>
+				</div>
+				<div class="email">
+					<span class="detail-title">Email:</span>
+					<span class="detail-value">
+						<a href="mailto:<?php echo carbon_get_theme_option('crb_theme_email') ?>">
+							<?php echo carbon_get_theme_option('crb_theme_email') ?>
+						</a>
+					</span>
+				</div>
+				<div class="copyright">
+					<span>© <?php echo date("Y"), " ", bloginfo('name') ?> | All rights reserved</span>
+				</div>
 			</div>
-			<div class="address">
-				<span><?php echo nl2br (carbon_get_theme_option('crb_theme_address')) ?></span>
-			</div>
-			<div class="telephone-no">
-				<span class="detail-title">Telephone:</span>
-				<span class="detail-value"><?php echo carbon_get_theme_option('crb_theme_telephone')?></span>
-			</div>
-			<div class="email">
-				<span class="detail-title">Email:</span>
-				<span class="detail-value">
-					<a href="mailto:<?php echo carbon_get_theme_option('crb_theme_email') ?>">
-						<?php echo carbon_get_theme_option('crb_theme_email') ?>
-					</a>
-				</span>
-			</div>
-			<div class="copyright">
-				<span>©<?php echo date("Y"), " ", bloginfo('name') ?> | All rights reserved</span>
-			</div>
-		</div>
-		<div class="explore">
-			<div class="newsletter">
-			</div>
-			<div class="links">
-				<?php
-				function snae_footer_nav($menu_location) {
-					$menu_name = wp_get_nav_menu_name($menu_location);
+			<div class="explore">
+				<div class="newsletter">
+				</div>
+				<div class="links">
+					<?php
+					function snae_footer_nav($menu_location) {
+						$menu_name = wp_get_nav_menu_name($menu_location);
 
-					if (!empty($menu_name)) {
-						echo ('<div class="footer-nav">');
-						echo ("<h1>". $menu_name. "</h1>");
-						wp_nav_menu( array(
-							'theme_location' => $menu_location,
-							'container-class' => 'footer_nav'
-						));
-						echo ("</div>");
+						if (!empty($menu_name)) {
+							echo ('<div class="footer-nav">');
+							echo ('<h2 class="footer-nav-title">'. $menu_name. '</h2>');
+							wp_nav_menu( array(
+								'theme_location' => $menu_location,
+								'container-class' => 'footer_nav'
+							));
+							echo ("</div>");
+						}
 					}
-				}
 
-				snae_footer_nav('footer_1');
-				snae_footer_nav('footer_2');
-				snae_footer_nav('footer_3');
-				snae_footer_nav('footer_4');
-				?>
+					snae_footer_nav('footer_1');
+					snae_footer_nav('footer_2');
+					snae_footer_nav('footer_3');
+					snae_footer_nav('footer_4');
+					?>
+				</div>
 			</div>
 		</div>
 	</footer>
