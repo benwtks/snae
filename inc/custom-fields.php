28f9aa7131672040e41235040014ebc14df149c1
Ben Watkins <contact@benwtks.com>
2020-05-08 01:44:07 +0100

    Add fields to theme options

--- inc/custom-fields.php
+++ inc/custom-fields.php
@@ -21,8 +21,10 @@ function hide_editor() {
 
 add_action( 'carbon_fields_register_fields', 'crb_attach_theme_options' );
 function crb_attach_theme_options() {
-    Container::make( 'theme_options', __( 'Theme Options', 'crb' ) )
+	Container::make( 'theme_options', __( 'Theme Options', 'crb' ) )
         ->add_fields( array(
             Field::make( 'textarea', 'crb_theme_address', 'Address' ),
-        ) );
+            Field::make( 'text', 'crb_theme_telephone', 'Phone number' ),
+            Field::make( 'text', 'crb_theme_email', 'Email' ),
+		) );
 }
