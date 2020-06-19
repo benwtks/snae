aa450107a5cb1287d8b4fb7287f3220531152e3f
Ben Watkins <contact@benwtks.com>
2020-06-19 03:54:42 +0100

    Modify custom fields for page types

--- inc/custom-fields.php
+++ inc/custom-fields.php
@@ -7,8 +7,8 @@ function hide_editor() {
     if( !isset( $post_id ) ) return;
 
     $title = get_the_title($post_id);
-     
-    if($title == 'Contact'){ // edit the template name
+
+    if($title == 'Contact'){
         remove_post_type_support('page', 'editor');
         remove_post_type_support('page', 'revisions');
         remove_post_type_support('page', 'custom-fields');
@@ -27,4 +27,33 @@ function crb_attach_theme_options() {
             Field::make( 'text', 'crb_theme_telephone', 'Phone number' ),
             Field::make( 'text', 'crb_theme_email', 'Email' ),
 		) );
+}
+
+add_action( 'carbon_fields_register_fields', 'crb_attach_artist_options' );
+function crb_attach_artist_options() {
+    Container::make( 'post_meta', 'Artist Details' )
+        ->where( 'post_type', '=', 'artists' )
+        ->add_fields( array(
+            Field::make( 'text', 'crb_job', 'Job title' ),
+            Field::make( 'textarea', 'crb_short_bio', 'Short Bio (80 character limit)' )
+                ->set_attribute( 'maxLength', 80 ),
+            Field::make( 'textarea', 'crb_bio', 'Bio' )
+		));
+
+    Container::make( 'post_meta', 'Links' )
+        ->where( 'post_type', '=', 'artists' )
+        ->add_fields( array(
+			Field::make( 'text', 'crb_website', 'Website/Portfolio'),
+			Field::make( 'text', 'crb_facebook', 'Facebook'),
+			Field::make( 'text', 'crb_twitter', 'Twitter'),
+			Field::make( 'text', 'crb_instagram', 'Instagram'),
+			Field::make( 'text', 'crb_etsy', 'Etsy')
+	    ));
+
+    Container::make( 'post_meta', 'Gallery' )
+        ->where( 'post_type', '=', 'artists' )
+        ->add_fields( array(
+            Field::make( 'media_gallery', 'crb_artist_gallery', 'Gallery' )
+		));
+
 }
