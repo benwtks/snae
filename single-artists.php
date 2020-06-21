25e6a68c0f6cfbbc302a62c9b5363d8aedad5b91
Ben Watkins <contact@benwtks.com>
2020-06-21 02:18:04 +0100

    Artist page

--- single-artists.php
+++ single-artists.php
@@ -16,11 +16,55 @@ get_header();
 		<main id="main" class="site-main">
 			<div class="artist-header">
 				<div class="about">
-					<img src="https://picsum.photos/200" alt="artist photo" />
+					<?php
+					require_once ABSPATH . '/wp-admin/includes/image.php';
+
+					function generate_cropped_path($photo_ID, $file) {
+						$photo_folder = substr($file, 0, strrpos($file, "/")) . "/";
+						$photo_file_type = strrchr($file, '.');
+
+						return "uploads/" . $photo_folder . $photo_ID . "-artist-photo" . $photo_file_type;
+					}
+
+					$photo_ID = carbon_get_post_meta(get_the_ID(), 'crb_artist_photo');
+					$file = wp_get_attachment_metadata($photo_ID)['file'];
+
+					$cropped_path = generate_cropped_path($photo_ID, $file);
+					$cropped_url = content_url($cropped_path);
+
+					if (!file_exists(WP_CONTENT_DIR . "/" . $cropped_path)) {
+						$image = wp_get_image_editor(WP_CONTENT_DIR . '/uploads/' . $file);
+
+						if (! is_WP_error($editor)) {
+							$image->resize(400,400, array( 'center', 'center'));
+							$image->save("wp-content/" . $cropped_path);
+						}
+					}
+
+					echo ("<img width='120' height='120' class='artist-photo' src='" . $cropped_url . "' alt='artist photo'/>");
+					?>
 					<div class="intro">
 						<h1 class="name"><?php echo(get_the_title()) ?></h1>
+						<span><?php echo(carbon_get_post_meta(get_the_ID(), 'crb_job')) ?></span>
 					</div>
 				</div>
+				<div class="links">
+					<!--
+					$link = carbon_get_post_meta(get_the_ID(), 'crb_instagram');
+					if ($link != null) {
+						echo  -->
+				</div>
+			</div>
+			<p><?php echo(carbon_get_post_meta(get_the_ID(), 'crb_bio')) ?></p>
+			<div class="gallery">
+				<?php
+				$gallery_photos = carbon_get_post_meta(get_the_ID(), 'crb_artist_gallery');
+
+				foreach ($gallery_photos as $photo_ID) {
+					echo wp_get_attachment_image($photo_ID);
+				}
+				?>
+			</div>
 		</main>
 	</div>
 
