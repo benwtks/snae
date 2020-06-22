9e9c01394696155f605dfa65a8ed3af1879e4115
Ben Watkins <contact@benwtks.com>
2020-06-22 03:21:41 +0100

    Begin developing workshop page

---
+++ single-workshops.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * The template for displaying a single workshop
+ *
+ * This is the template that displays a workshop using
+ * the custom workshop page type
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package snae
+ */
+
+get_header();
+?>
+
+
