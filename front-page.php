c14368cf27fd3b4999a40d0431721fcec68bad66
Ben Watkins <contact@benwtks.com>
2020-07-04 15:33:05 +0100

    Work on homepage

--- front-page.php
+++ front-page.php
@@ -8,30 +8,43 @@
 get_header();
 
 while ( have_posts() ) : the_post(); ?>
-<div id="primary" class="content-area content-wrapper page-wrapper">
+<div id="primary" class="content-area front-page">
 	<main id="main" class="site-main">
 		<div class="home-top">
-			<h1><?php echo carbon_get_post_meta(get_the_ID(), 'crb_homepage_top_title')?></h1>
-			<p><?php echo carbon_get_post_meta(get_the_ID(), 'crb_homepage_top_body')?></p>
+			<div class="top-wrapper content-wrapper page-wrapper">
+				<h1><?php echo carbon_get_post_meta(get_the_ID(), 'crb_homepage_top_title')?></h1>
+				<p><?php echo carbon_get_post_meta(get_the_ID(), 'crb_homepage_top_body')?></p>
+			</div>
 		</div>
 		<?php
 		$featured_pages = carbon_get_post_meta(get_the_ID(), 'crb_homepage_features');
 
-		foreach ($featured_pages as $page) {
-			$title = get_the_title($page['crb_homepage_feature']);
-			$img_url = wp_get_attachment_image_url($page['crb_homepage_feature_img']);
+		if ($featured_pages) {
+			echo '<div class="featured-pages">';
 
-			echo '<div class="home-feature">';
-			echo '<img src="' . $img_url . '" alt="page image">';
-			echo '<h2>' . $title . '</h2>';
-			echo '<p>' . $page['crb_homepage_feature_body'] . '</p>';
+			foreach ($featured_pages as $page) {
+				$title = get_the_title($page['crb_homepage_feature']);
+				$img_url = wp_get_attachment_url($page['crb_homepage_feature_img']);
+
+				echo '<div class="home-feature">';
+				echo '<div class="home-feature-wrapper content-wrapper page-wrapper">';
+				echo '<img src="' . $img_url . '" alt="page image">';
+				echo '<div class="feature-details">';
+				echo '<h2>' . $title . '</h2>';
+				echo '<p>' . $page['crb_homepage_feature_body'] . '</p>';
+				echo '</div>';
+				echo '</div>';
+				echo '</div>';
+			}
+
+			echo '</div>';
 			echo '</div>';
 		}
 
 		$workshops = carbon_get_post_meta(get_the_ID(), 'crb_homepage_workshops');
 
 		if ($workshops) {
-			echo '<div class="home-workshops">';
+			echo '<div class="home-workshops page-wrapper content-wrapper">';
 			echo '<h2>Workshops</h2>';
 			echo '<div class="workshop-previews">';
 			foreach ($workshops as $workshop) {
@@ -48,7 +61,7 @@ while ( have_posts() ) : the_post(); ?>
 		$artists = carbon_get_post_meta(get_the_ID(), 'crb_homepage_artists');
 
 		if ($artists) {
-			echo '<div class="home-artistss">';
+			echo '<div class="home-artists page-wrapper content-wrapper">';
 			echo '<h2>Artists</h2>';
 			echo '<div class="artist-previews">';
 			foreach ($artists as $artist) {
@@ -67,7 +80,7 @@ while ( have_posts() ) : the_post(); ?>
 		$action_links = carbon_get_post_meta(get_the_ID(), 'crb_call_to_action_links');
 
 		if ($action_title && $action_body) {
-			echo '<div class="home-call-to-action">';
+			echo '<div class="home-call-to-action page-wrapper content-wrapper">';
 			echo '<h2>' . $action_title . '</h2>';
 			echo '<p>' . $action_body . '</p>';
 
